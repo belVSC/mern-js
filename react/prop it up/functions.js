e828c20412f97e907b72ff50f5dc2a454a574752
Ben <benelane241@gmail.com>
2023-05-01 14:39:08 -0400

    first commit

---
+++ react/prop it up/functions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const add = (props) => {
+    return (
+        <div>
+            this is our first class component.
+        </div>
+    )
+}
+
+export default SomeFunctionalComponent;
