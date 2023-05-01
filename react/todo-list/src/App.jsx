1d31acd143effa3450aef1c8d32510472c8956f9
Ben <benelane241@gmail.com>
2023-05-01 15:04:58 -0400

    Added displaylist

--- react/todo-list/src/App.jsx
+++ react/todo-list/src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css';
 import TodoForm from './components/TodoForm';
+import DisplayList from './components/DisplayList';
 
 function App() {
   return (
     <div className="App">
+      <DisplayList/>
       <TodoForm/>
     </div>
   );
